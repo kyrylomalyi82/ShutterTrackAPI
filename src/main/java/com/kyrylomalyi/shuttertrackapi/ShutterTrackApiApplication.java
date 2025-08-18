4d8e6084c5a7fe97ad7d9b6a25308a721080923a
kyrylomalyi81 <kyrylomalyi81@gmail.com>
2025-08-18 15:06:09 +0200

    chore: initialize Spring Boot project with basic setup

---
+++ src/main/java/com/kyrylomalyi/shuttertrackapi/ShutterTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.kyrylomalyi.shuttertrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShutterTrackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShutterTrackApiApplication.class, args);
+    }
+
+}
